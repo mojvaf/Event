eb0b6b3e7ff784814fa12a4f4ee6e6ae0ec86c39
mojvaf <mvafa44@gmail.com>
2021-05-04 13:09:58 +0200

    fixed some bugs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { AppComponent } from './app.component';
 import { DetailEventComponent } from './detail-event/detail-event.component';
+import { EventComponent } from './event/event.component';
 
 const routes: Routes = [
   {
   path: '',  component:AppComponent
 },
 {
-  path: 'event/:id', component:DetailEventComponent
+  path: 'event/:id', component:EventComponent
+},
+{
+  path: 'detail', component:DetailEventComponent
 }
 ];
 
@@ -20,5 +24,5 @@ const routes: Routes = [
 export class AppRoutingModule { }
 export const routingComponents = [
          DetailEventComponent,
-         AppComponent
+         EventComponent
 ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutUsComponent } from './about-us/about-us.component';
-import { EventComponent } from './event/event.component';
-import {DetailEventComponent} from './detail-event/detail-event.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutUsComponent,
-    EventComponent,
-    DetailEventComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
